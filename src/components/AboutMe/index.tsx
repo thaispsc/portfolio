3a44adee5b097f3ce339859d512b18d5b2c22047
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-07 23:32:08 -0300

    Alteracao secao introduction e about me

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,13 +1,13 @@
-import "./styles.css";
+import "../AboutMe/styles.css"
 
 function AboutMe() {
   return (
       <div>
-          <h1>About Me</h1>
+          {/* <h1>About Me</h1>
           <div>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum neque excepturi vero! Qui voluptatum amet, voluptates accusantium obcaecati aut quas veritatis? Cupiditate aperiam at non veritatis! Dignissimos beatae facere atque, voluptatem voluptatibus at sequi eius praesentium illum, nulla cum. Sit, consectetur ad quasi adipisci dolores inventore quam iste quibusdam.</p>
           <img src="/public/logo512.png" alt="" />
-          </div>
+          </div> */}
       </div>
   );
 }
--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,10 +1,11 @@
-import "./styles.css";
+import "../Introduction/styles.css";
 
 function Introduction() {
   return (
-      <div>
-          <h1>Thais Cavalcante</h1>
-          <h4>Desenvolvedora Front-End</h4>
+      <div className="introduction">
+        <h5>Olá, meu nome é</h5>
+          <h1>Thais Cavalcante.</h1>
+          <h4>Eu sou desenvolvedora Web Front-End.</h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestiae in tenetur ullam nihil? Accusamus quisquam cum repudiandae optio officia, cupiditate reprehenderit, ut odit quidem pariatur inventore, nemo mollitia! Voluptates.</p>
       </div>
   );
