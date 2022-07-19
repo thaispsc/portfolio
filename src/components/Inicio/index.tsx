9d24189d1f350368558012138baa60ff4a4fe978
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-18 22:33:51 -0300

    Alteração minima

--- src/components/Inicio/index.tsx
+++ src/components/Inicio/index.tsx
@@ -10,7 +10,7 @@ const Inicio: React.FC = () => {
           <p className='text-pink-200'>Olá, meu nome é</p>
           <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Thais Cavalcante.</h1>
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Desenvolvedora Front-End.</h2>
-          <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas ullam debitis dicta aperiam, error cupiditate distinctio. Iusto accusantium tenetur ab.</p>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, id? At libero eum natus facere modi accusantium minus! Totam nam asperiores voluptatibus magnam, ut est.</p>
           <div>
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Mais informações
             <span className='group-hover:rotate-90 duration-300'>
