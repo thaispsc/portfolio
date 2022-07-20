b3193cd565b0f71fbbfb37b5d17d555a2a51987f
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-19 21:57:42 -0300

    Alteração habilidades

--- src/components/Habilidades/index.tsx
+++ src/components/Habilidades/index.tsx
@@ -37,22 +37,6 @@ const Habilidades: React.FC = () => {
                       <img className='w-20 mx-auto' src={Reactjs} alt="React icon" />
                       <p className='my-4 '>ReactJS</p>
                   </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <img className='w-20 mx-auto' src={Node} alt="Node.JS icon" />
-                      <p className='my-4 '>Node.Js</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <img className='w-20 mx-auto' src={Github} alt="Github icon" />
-                      <p className='my-4 '>GitHub</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
-                      <p className='my-4 '>-</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <img className='w-20 mx-auto' src={Mongo} alt="Mongo icon" />
-                      <p className='my-4 '>-</p>
-                  </div>
               </div>
           </div>
       </div>
