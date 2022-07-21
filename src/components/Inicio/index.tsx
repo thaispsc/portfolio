7c633f61002e7b2ad9ddc4b0aea9ed7efa9a8d63
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-20 23:31:52 -0300

    Alteração Sobre mim

--- src/components/Inicio/index.tsx
+++ src/components/Inicio/index.tsx
@@ -7,10 +7,10 @@ const Inicio: React.FC = () => {
       <div id="inicio" className='w-full h-screen bg-[#0a192f]'>
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-          <p className='text-pink-200'>Olá, meu nome é</p>
+          <p className='text-pink-600'>Olá, meu nome é</p>
           <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Thais Cavalcante.</h1>
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Desenvolvedora Front-End.</h2>
-          <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, id? At libero eum natus facere modi accusantium minus! Totam nam asperiores voluptatibus magnam, ut est.</p>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>Engenheira civil por formação e apaixonada por tecnologia. Tenho sólidas habilidades de resolução de problemas e o desejo de sempre aprender mais.</p>
           <div>
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Mais informações
             <span className='group-hover:rotate-90 duration-300'>
